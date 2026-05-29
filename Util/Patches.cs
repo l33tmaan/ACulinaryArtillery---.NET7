49858fe724b216b055a450171931f2701b160a43
Xboxn <conorhalpin21@gmail.com>
2026-05-29 23:48:34 +0100

    Cleaned up implementation

--- Util/Patches.cs
+++ Util/Patches.cs
@@ -583,11 +583,12 @@ namespace ACulinaryArtillery
         public static bool mulitPie(ref bool __result, ref InventoryBase ___inv, BlockEntityPie __instance, ItemSlot slot, IPlayer? byPlayer = null)
         {
             ICoreClientAPI? capi = __instance.Api as ICoreClientAPI;
-            if (slot.Itemstack.Collectible is BlockLiquidContainerBase liquidContainer && !liquidContainer.AllowHeldLiquidTransfer) { 
+            ILiquidSource? container = slot.Itemstack.Collectible.GetCollectibleInterface<ILiquidSource>();
+            if (container?.AllowHeldLiquidTransfer == false)
+            {
                 __result = false;
                 return false;
             }
-            ILiquidSource? container = slot.Itemstack.Collectible as ILiquidSource;
             ItemStack contentStack = container?.GetContent(slot.Itemstack) ?? slot.Itemstack;
             InPieProperties? pieProps = contentStack.ItemAttributes?["inPieProperties"]?.AsObject<InPieProperties?>(null, contentStack.Collectible.Code.Domain);
 
